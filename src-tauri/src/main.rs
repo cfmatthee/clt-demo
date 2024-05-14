3785e57d2c11423db46e31bbe04134dba94914e6
Chris Matthee <cfmatthee@gmail.com>
2024-05-14 14:01:59 +0200

    send commands from frontend to backend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,14 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[tauri::command]
+fn run_command(command: &str) {
+    println!("Received command {}", command);
+}
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![run_command])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
