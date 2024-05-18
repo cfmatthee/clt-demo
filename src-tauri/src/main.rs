aac29c53758a5da0dc42d91292aebd0e3aa27c0b
Chris Matthee <cfmatthee@gmail.com>
2024-05-18 21:56:27 +0200

    change to symmetrical distributions

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -18,8 +18,8 @@ use tauri::Manager;
 #[derive(Serialize)]
 struct Histogram {
     data: Vec<f32>,
-    min: u32,
-    max: u32,
+    min: i32,
+    max: i32,
     mean: f32,
     stdev: f32,
     guassian: Vec<f32>,
@@ -27,9 +27,9 @@ struct Histogram {
 
 #[derive(Debug)]
 struct Data {
-    data: Vec<u32>,
-    min: u32,
-    max: u32,
+    data: Vec<i32>,
+    min: i32,
+    max: i32,
 }
 
 struct AppState(Mutex<Data>);
@@ -71,25 +71,25 @@ impl Data {
     }
 
     fn add_rectangular(&mut self) {
-        const MAX: u32 = 10;
+        const SEMI_RANGE: i32 = 5;
         let mut rng = rand::thread_rng();
         for item in self.data.iter_mut() {
-            *item += rng.gen_range(1..=MAX);
+            *item += rng.gen_range(-SEMI_RANGE..=SEMI_RANGE);
         }
-        self.min += 1;
-        self.max += MAX;
+        self.min -= SEMI_RANGE;
+        self.max += SEMI_RANGE;
     }
 
     fn add_ushaped(&mut self) {
-        const MAX: u32 = 10;
+        const SEMI_RANGE: i32 = 5;
         let mut rng = rand::thread_rng();
         for item in self.data.iter_mut() {
             let x: f32 = rng.gen::<f32>();
-            let y: f32 = (PI * x - PI / 2.).sin() * 4.999999 + 5.;
-            *item += y.ceil() as u32;
+            let y: f32 = (PI * x - PI / 2.).sin() * (SEMI_RANGE as f32 + 0.5);
+            *item += y.max(-5.).min(5.).round() as i32;
         }
-        self.min += 1;
-        self.max += MAX;
+        self.min -= SEMI_RANGE;
+        self.max += SEMI_RANGE;
     }
 
     fn create_histogram(&self) -> Histogram {
@@ -110,12 +110,12 @@ impl Data {
         let mean: f32 = histogram
             .iter()
             .enumerate()
-            .map(|(x, p)| ((x + self.min as usize) as f32) * p)
+            .map(|(x, p)| ((x as i32 + self.min) as f32) * p)
             .sum();
         let stdev: f32 = histogram
             .iter()
             .enumerate()
-            .map(|(x, p)| (((x + self.min as usize) as f32) - mean).powf(2.) * p)
+            .map(|(x, p)| (((x as i32 + self.min) as f32) - mean).powf(2.) * p)
             .sum::<f32>()
             .sqrt();
         let factor: f32 = 1. / (stdev * (2. * PI).sqrt());
@@ -188,8 +188,8 @@ mod tests {
         let mut data = setup();
         data.add_rectangular();
         assert_eq!(data.data.len(), 1_000_000);
-        assert_eq!(data.data.iter().min(), Some(&1));
-        assert_eq!(data.data.iter().max(), Some(&10));
+        assert_eq!(data.data.iter().min(), Some(&-5));
+        assert_eq!(data.data.iter().max(), Some(&5));
     }
 
     #[test]
@@ -197,8 +197,8 @@ mod tests {
         let mut data = setup();
         data.add_ushaped();
         assert_eq!(data.data.len(), 1_000_000);
-        assert_eq!(data.data.iter().min(), Some(&1));
-        assert_eq!(data.data.iter().max(), Some(&10));
+        assert_eq!(data.data.iter().min(), Some(&-5));
+        assert_eq!(data.data.iter().max(), Some(&5));
     }
 
     #[test]
@@ -219,17 +219,17 @@ mod tests {
 
         data.add_rectangular();
         let histogram = data.create_histogram();
-        assert_eq!(histogram.data.len(), 10);
+        assert_eq!(histogram.data.len(), 11);
         assert_abs_diff_eq!(histogram.data.iter().sum::<f32>(), 1.0, epsilon = 0.000001);
 
         data.add_rectangular();
         let histogram = data.create_histogram();
-        assert_eq!(histogram.data.len(), 19);
+        assert_eq!(histogram.data.len(), 21);
         assert_abs_diff_eq!(histogram.data.iter().sum::<f32>(), 1.0, epsilon = 0.000001);
 
         data.add_rectangular();
         let histogram = data.create_histogram();
-        assert_eq!(histogram.data.len(), 28);
+        assert_eq!(histogram.data.len(), 31);
         assert_abs_diff_eq!(histogram.data.iter().sum::<f32>(), 1.0, epsilon = 0.000001);
     }
 }
