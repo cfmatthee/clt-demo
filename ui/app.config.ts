6c3cda861844ccc91160430a2460a14fa96497d6
Chris Matthee <cfmatthee@gmail.com>
2024-10-16 16:01:33 +0200

    update dependencies

--- ui/app.config.ts
+++ ui/app.config.ts
@@ -2,10 +2,4 @@ import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
   ssr: false,
-  server: {
-    static: true,
-    prerender: {
-      crawlLinks: true,
-    },
-  },
 });
