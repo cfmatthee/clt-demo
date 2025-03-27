1fcfe1ce40e6f80bcf6b7d696833025b49615137
Chris Matthee <cfmatthee@gmail.com>
2025-03-27 13:47:09 +0200

    set up control panel linked to backend

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -1,47 +1,51 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import { invoke } from "@tauri-apps/api/core";
+  import Controls from "./lib/Controls.svelte";
+
+  type Data = {
+    data: number[];
+    guassian: number[];
+    min: number;
+    max: number;
+    mean: number;
+    stdev: number;
+  };
+
+  let data = $state<Data>({
+    data: [],
+    guassian: [],
+    min: 0,
+    max: 0,
+    mean: 0,
+    stdev: 0,
+  });
+
+  const handleClick = (cmd: string) => {
+    invoke<Data>("run_command", { command: cmd || "" }).then((new_data) => {
+      data = new_data;
+    });
+  };
+
+  $effect(() => {
+    $inspect(data);
+  });
 </script>
 
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <div class="histogram"></div>
+  <Controls onclick={handleClick} />
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  main {
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+    height: 100vh;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+
+  .histogram {
+    background-color: #ccc;
+    flex-grow: 1;
   }
 </style>
---
+++ ui/src/lib/Controls.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  interface Props {
+    onclick?: (cmd: string) => void;
+  }
+  let { onclick }: Props = $props();
+
+  const commands = {
+    clear: "Clear chart",
+    rectangular: "Rectangular",
+    ushaped: "U-shaped",
+  };
+
+  const handleClick = (cmd: string) => {
+    if (onclick) onclick(cmd);
+  };
+</script>
+
+<div class="container">
+  {#each Object.entries(commands) as command}
+    <button
+      id={command[0]}
+      class={[command[0] != "clear" && "primary"]}
+      onclick={() => handleClick(command[0])}
+    >
+      {command[1]}
+    </button>
+  {/each}
+</div>
+
+<style>
+  .container {
+    display: flex;
+    flex-direction: row;
+    gap: 15px;
+    justify-content: center;
+    padding: 5px;
+  }
+
+  button {
+    border: 1px solid black;
+    border-radius: 10px;
+    padding: 7px 20px;
+  }
+
+  .primary {
+    background-color: var(--primary-color);
+    color: white;
+  }
+</style>
